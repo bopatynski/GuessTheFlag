64d82913b1c77f4e7090dd36981078871a3b7ab7
bopatynski <bopatynski@gmail.com>
2022-04-21 23:33:41 -0400

    yabadabadoo.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score = score + 1
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack{
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
+                .ignoresSafeArea()
+
+            VStack(spacing: 15) {
+                Spacer()
+                Text("Guess the Flag")
+                        .font(.largeTitle.weight(.bold))
+                        .foregroundColor(.white)
+                Spacer()
+                Spacer()
+                Text("Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                Spacer()
+                VStack {
+                        
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary).font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .foregroundColor(.white)
+                            .font(.title
+                            .bold())
+
+                    }.frame(maxWidth: .infinity)
+                    .padding(.vertical, 20)
+                    .padding(.horizontal,20)
+                    .background(.regularMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+
+                    ForEach(0..<3) { number in
+                        Button {
+                           flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 10)
+                        }
+                    }
+            }.padding()
+            
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(score)")
+        }
     }
 }
 
